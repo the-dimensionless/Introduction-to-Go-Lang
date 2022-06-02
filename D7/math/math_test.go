04ab3da829af3d3ecd2304de4725a3a4c7acfd5f
the-dimensionless <sumit.singh1@brillio.com>
2022-06-02 12:59:46 +0530

    test

--- D7/math/math_test.go
+++ D7/math/math_test.go
@@ -6,20 +6,20 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestAdd(t *testing.T) {
+// func TestAdd(t *testing.T) {
 
-	got := Add(4, 6)
-	want := 10
+// 	got := Add(4, 6)
+// 	want := 10
 
-	if got != want {
-		t.Errorf("got %q, wanted %q", got, want)
-	}
-}
+// 	if got != want {
+// 		t.Errorf("got %q, wanted %q", got, want)
+// 	}
+// }
 
 func TestSomething(t *testing.T) {
 
 	var a string = "Hello"
-	var b string = "Hello2"
+	var b string = "Hello"
 
 	assert.Equal(t, a, b, "The two words should be the same.")
 
--- D7/math/sub_test.go
+++ D7/math/sub_test.go
@@ -1,15 +1,24 @@
 package math
 
-import (
-	"testing"
-)
+import "testing"
 
-func TestSub(t *testing.T) {
+// arg1 means argument 1 and arg2 means argument 2, and the expected stands for the 'result we expect'
+type addTest struct {
+	arg1, arg2, expected int
+}
 
-	got := Subtract(40, 10)
-	want := 30
+var addTests = []addTest{
+	addTest{2, 3, 5},
+	addTest{4, 8, 12},
+	addTest{6, 9, 15},
+	addTest{3, 10, 13},
+}
 
-	if got != want {
-		t.Errorf("got %q, wanted %q", got, want)
+func TestAdd(t *testing.T) {
+
+	for _, test := range addTests {
+		if output := Add(test.arg1, test.arg2); output != test.expected {
+			t.Errorf("Output %q not equal to expected %q", output, test.expected)
+		}
 	}
 }
